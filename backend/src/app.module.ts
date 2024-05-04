ef80b9fb8617a7d3265e6d4c8cdd34921de27510
Eason Chai <echai2905@gmail.com>
2024-05-05 02:57:30 +1000

    feat: Add prisma module with exports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { RequestModule } from './request/request.module';
+import { Web3Module } from './web3/web3.module';
 
 @Module({
-  imports: [PrismaModule, RequestModule],
+  imports: [PrismaModule, RequestModule, Web3Module],
   controllers: [AppController],
   providers: [AppService],
 })
